0ec5ea75aa9f04345094cc748c061a42e90be621
Mac Schmitz <mac.schmitz@att.net>
2022-08-02 01:39:14 -0400

    set up User model

---
+++ routes/auth.js
@@ -0,0 +1,16 @@
+const router = require("express").Router();
+const User = require("../models/User")
+
+//REGISTER
+router.post("/register", async (req, res) => {
+    const user = await new User({
+        username: 'John',
+        email: 'john@gmail.com',
+        password: '123456'
+    })
+
+    await user.save();
+    res.send("ok")
+})
+
+module.exports = router
