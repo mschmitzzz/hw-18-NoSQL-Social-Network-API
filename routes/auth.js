5ce9ad3d73c7e5177fc2d2ed9f31a016c0d3e2cd
Mac Schmitz <mac.schmitz@att.net>
2022-08-02 11:02:52 -0400

    trying to add user registration but getting no connection to /api/auth/register

--- routes/auth.js
+++ routes/auth.js
@@ -1,16 +1,37 @@
 const router = require("express").Router();
 const User = require("../models/User")
+const bcrypt = require("bcrypt");
 
 //REGISTER
 router.post("/register", async (req, res) => {
-    const user = await new User({
-        username: 'John',
-        email: 'john@gmail.com',
-        password: '123456'
-    })
+    // const user = await new User ({
+    //     username: "john",
+    //     email: "john@gmail.com",
+    //     password: "123456"
+    // })
 
-    await user.save();
-    res.send("ok")
+    // await user.save();
+    // res.send("ok")
+
+
+    try {
+        // generate new hashed password
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(req.body.password, salt);
+
+        //create new user
+        const newUser = new User ({
+            username: req.body.username,
+            email: req.body.email,
+            password: hashedPassword,
+        });
+
+        //save user and respond
+        const user = await newUser.save();
+        res.status(200).json(user)
+    } catch (err) {
+        console.log(err)
+    }
 })
 
 module.exports = router
